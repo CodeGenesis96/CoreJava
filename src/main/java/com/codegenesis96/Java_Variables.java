c99e06122ad8a46e4bb3e15d8d98614596afe914
IT18085372 <vinodmahes96@gmail.com>
2024-07-04 15:37:31 +0530

    First Commit

---
+++ src/main/java/com/codegenesis96/Java_Variables.java
@@ -0,0 +1,23 @@
+package com.codegenesis96;
+
+public class Java_Variables {
+
+    boolean booleanV = true;                    // size-> 1 bit
+    byte byteV = 100;                           // size-> 1 byte, range -> -128 to +127
+    short shortV = 30000;                       // size-> 2 bytes. range -> -32768 to +32769
+    int intV = 1230000;                         // size-> 4 bytes. range -> -2 billion to 2 billion
+    long longV = 20000000;                      // size-> 8 bytes, range -> -9 quintillion to 9 quintillion
+
+    float floatV = 209.7889f;                   // size-> 4 bytes, range -> up to 6-7 decimal places
+    double doubleV = 3.14;                      // size-> 8 bytes, range -> up to 15 decimal places
+
+    char charV = '@';                           // size-> 2 bytes, value -> single character/letter/ASCII value
+    String StringV = "Bro";                     // size-> varies, values -> a sequence of characters
+
+    /*
+    declaration -> int x
+    assignment -> x = 123;
+    initialization(declaration + assignment) -> int x = 123;
+     */
+
+}
